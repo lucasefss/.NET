8da7b08734586c9abe9071c34d21358a08a9bf25
lucasefss <lucasef96@gmail.com>
2022-02-02 23:00:06 -0300

    abstracao

---
+++ abstracao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using modelos;
+
+    namespace method {
+
+        class cadastro 
+        {
+            static void Main (string [] args) 
+            {                
+            
+            Pessoa p1 = new Pessoa(); // Cria um objeto p1 do tipo Pessoa
+            
+            p1.Nome = "Lucas";
+            p1.Idade = 20;
+
+            p1.Apresentar();
+
+        }
+    }
+}
+
