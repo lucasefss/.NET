1b0f8b501aca512f8581d831d10efb9aa84959d8
Lucas Ferreira <96503767+lucasefss@users.noreply.github.com>
2022-02-15 14:39:57 -0300

    Array-Sort
    Explicação simples de Array.Sort();

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    namespace exemplo 
+    {
+        class exemplo2 
+        {
+            
+            static void Main (string [] args) 
+            {
+                operacoesarray op = new operacoesarray();
+            
+                int [] array = new int [5] {5,7,8,4,1};
+            
+                op.OrganizarArray(ref array);
+           
+                op.imprimirarray(array);
+            }
+            
+            
+        }
+    }
---
+++ method.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace exemplo
+{
+    public class operacoesarray 
+    {
+        public void OrganizarArray(ref int [] array)
+        {
+            Array.Sort(array);
+        }
+
+        public void imprimirarray(int [] array) 
+        {
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     System.Console.WriteLine(array[i]);  
+            // }
+
+            var linha = string.Join(", ", array);
+            System.Console.WriteLine(linha);
+        }
+    }
+}
